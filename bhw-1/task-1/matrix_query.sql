c4b60c907e2f635b511bca1d889a88a203705bd2
Mikhail Tsion <tsionmichel1999@gmail.com>
2018-10-24 22:09:05 +0300

    Update matrix_query.sql

--- bhw-1/task-1/matrix_query.sql
+++ bhw-1/task-1/matrix_query.sql
@@ -1,11 +1,14 @@
 /* 2 */
 select row_no, column_no, element_val from matrix_data where matrix_id = $1;
 
+
 /* 3 */
 select column_no as row_no, row_no as column_no, element_val from matrix_data where matrix_id = $1;
 
+
 /* 4 */
 select row_no, column_no, element_val * $1 from matrix_data where matrix_id = $0;
+
 
 /* 5 */
 select case
@@ -18,6 +21,7 @@ select case
 
 from matrix_data where matrix_id = $1;
 
+
 /* 6 */
 select
   case
@@ -28,6 +32,7 @@ select
 
   end as answer
 from matrix_data where matrix_id = $1;
+
 
 /* 7 */
 select
@@ -44,6 +49,66 @@ select
       'yes'
     end as answer;
 
+/* #8 */
+select
+  MATRIX_DATA.row_no
+  , MATRIX_DATA.column_no
+  , MATRIX_DATA.element_val
+from
+  MATRIX_DATA
+where 1=1
+  and MATRIX_DATA.matrix_id = $1 /* ADD PARAM THERE */
+  and MATRIX_DATA.column_no % 2 = 0
+  and MATRIX_DATA.row_no % 2 = 1
+order by
+  row_no
+  , column_no;
+
+
+/* #10 */
+select
+  case
+    when
+      (select max(row_no) from matrix_data where matrix_id = $1)
+        = (select max(row_no) from matrix_data where matrix_id = $2)
+      and (select max(column_no) from matrix_data where matrix_id = $1)
+            = (select max(column_no) from matrix_data where matrix_id = $2)
+      then 'POSSIBLE'
+    else
+       'IMPOSSIBLE'
+  end;
+
+
+/* #11 */
+select
+  A.column_no
+  , A.row_no
+  , A.element_val + B.element_val as result
+from
+  MATRIX_DATA A
+join
+  MATRIX_DATA B
+  on
+    A.matrix_id=$1
+    and B.matrix_id=$2
+where 1=1
+  and A.row_no=B.row_no
+  and A.column_no=B.column_no
+  and (
+    select
+      case
+        when
+          (select max(row_no) from matrix_data where matrix_id = $1)
+            = (select max(row_no) from matrix_data where matrix_id = $2)
+          and (select max(column_no) from matrix_data where matrix_id = $1)
+              = (select max(column_no) from matrix_data where matrix_id = $2)
+          then true
+        else
+           false
+      end
+  );
+
+
 /* 12 */
 select
   case
@@ -54,6 +119,7 @@ select
 
   end as answer
 from matrix_data as matr1, matrix_data as matr2 where matr1.matrix_id = $1 and matr2.matrix_id = $2;
+
 
 /* 13 */
 
